a36f3959703884778a471e5559faae4fe1a20447
mangesh sambare <sambaremangesh123@gmail.com>
2026-09-15 00:14:04 +0530

    Remove profile photo from About section

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -59,16 +59,7 @@ export default function AboutSection() {
               Open to Opportunities
             </div>
 
-            {/* Profile photo */}
-            <div className="relative w-44 h-44 rounded-full overflow-hidden border-4 border-[#1a2333] shadow-[0_0_25px_rgba(0,240,255,0.3)]">
-              <Image
-                src="/profile.png"
-                alt="Mangesh Sambare"
-                fill
-                sizes="176px"
-                className="object-cover"
-              />
-            </div>
+
 
             {/* Name */}
             <div className="text-center">
